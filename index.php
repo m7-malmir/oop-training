9adf4cb87234931c53f644eb7cb3063c33d1ee65
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-03 23:02:06 +0330

    third commit add

--- index.php
+++ index.php
@@ -1,23 +1,59 @@
-<?php
-// spl_autoload_register('autoload');
-// function autoload($classname){
-//   include_once 'classes/'.$classname.'.php';
-// }
+<html>
 
+<head>
+  <title>Sling Academy</title>
+  <style>
+    /* We will get the position of this element */
+    #example-element {
+      /* width: 20px;
+      height: 10px;
+      background-color: lightblue;
+      margin: 50px; */
+    }
 
-function coor($x,$y){
-  echo $x."<div class='dots'></div>".$y;
-}
-$x=20;
-$y=10;
-coor($x,$y);
+    /* The result will be displed within this element */
+    #output {
+      font-size: 30px;
+      color: blue;
+    }
+  </style>
 
+</head>
 
+<body>
+  <h2>Welcome to Sling Academy</h2>
+  <button id="show-position">Show position</button>
+  <div id="example-element">*</div>
+  <div id="output"></div>
 
+  <script>
+    // Put your JS code here
+    // Get the elements by their ids
+    const button = document.getElementById("show-position");
+    const element = document.getElementById("example-element");
+    const output = document.getElementById("output");
 
+    // Define a function to display the position of the element
+    const displayPosition = () => {
+      // Use getBoundingClientRect() to get the position of the element
+      const rect = element.getBoundingClientRect();
 
+      const x = rect.left + window.scrollX;
+      const y = rect.top + window.scrollY;
 
+      // Create a string with the position information
+      const position = `X: ${x} <br>Y: ${y}`;
 
+      // Display the position in the output element
+      output.innerHTML = position;
+    }
 
+    // Add an event listener to the button to call displayPosition on click
+    button.addEventListener("click", displayPosition);
 
+    // Add an event listener to the window to call displayPosition on resize
+    //window.addEventListener("resize", displayPosition);
+  </script>
+</body>
 
+</html>
