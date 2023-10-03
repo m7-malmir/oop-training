8731fd2404b1dfa2c6c7f5484d0bcf29ca674ef6
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-03 21:19:27 +0330

    second commit

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-spl_autoload_register('autoload');
-function autoload($classname){
-  include_once 'classes/'.$classname.'.php';
-}
+// spl_autoload_register('autoload');
+// function autoload($classname){
+//   include_once 'classes/'.$classname.'.php';
+// }
 
+
+function coor($x,$y){
+  echo $x."<div class='dots'></div>".$y;
+}
+$x=20;
+$y=10;
+coor($x,$y);
 
 
 
