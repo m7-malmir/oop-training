e1de6d26259a66c96430a2687b5d995dda973245
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-05 23:03:43 +0330

    some file added

--- test.php
+++ test.php
@@ -9,39 +9,26 @@
   <div id="output"></div>
 
 <?php
-function start($xy,$z){
-    $stri = (string)$xy;
-    $x = $stri[0];
-    $y = $stri[1];
-    $strz = (string)$z;
-    $j = $strz[0];
-    $k = $strz[1];
-    $a=$x- $j;
-    $b=$y-$k;
-    
+// function start($xy,$z){
+//     $stri = (string)$xy;
+//     $x = $stri[0];
+//     $y = $stri[1];
+//     $strz = (string)$z;
+//     $j = $strz[0];
+//     $k = $strz[1];
+//     $a= $j-$x;
+//     $b= $k-$y;
+//     $des=[$a,$b];
+//     print_r($des);
+// }
+// start(13,26);
+$x=1;
+$d=6;
+function right(){
+  global $x;
+    $x+1;
 }
-start(23,56);
+// function up(){
+//   $y+1;
+// }
 ?>
-
-  <script>
-
-    const button = document.getElementById("show-position");
-    const element = document.getElementById("example-element");
-    const output = document.getElementById("output");
-
-    const displayPosition = () => {
-    
-      const rect = element.getBoundingClientRect();
-      const x = rect.left + window.scrollX;
-      const y = rect.top + window.scrollY;
-      
-      const position = `X: ${x} <br>Y: ${y}`;
-     
-      output.innerHTML = position;
-    }
-
-    button.addEventListener("click", displayPosition);
-
-  </script>
-</body>
-</html>
