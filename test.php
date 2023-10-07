bba05441949c5275fb2177fc457264c8055b4a09
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-07 23:49:34 +0330

    new function is added

--- test.php
+++ test.php
@@ -1,14 +1,13 @@
 <html>
 <head>
-
 </head>
 <body>
   <h2>Welcome to Sling Academy</h2>
   <button id="show-position">Show position</button>
   <div id="example-element">*</div>
   <div id="output"></div>
-
 <?php
+session_start();
 function start($xy,$z){
     $stri = (string)$xy;
     $x = $stri[0];
@@ -19,15 +18,26 @@ function start($xy,$z){
     $a= $j-$x;
     $b= $k-$y;
     $des=[$a,$b];
-    print_r($des);
+    $_SESSION['des']=$des;
+    print_r( $_SESSION['des']);
 }
 start(13,26);
-$x=1;
-$d=6;
+// $x=1;
+// $d=6;
 function right(){
-  global $x;
-  global $d;
-    $x=$x+1;
+  $x=$_SESSION['des'][0]+=1;
+  $d=$_SESSION['des'][1];
+    if($x==$d){
+      return $d;
+    }elseif($x>$d){
+      return false;
+    }else{
+      return $x;
+    }
+}
+function left(){
+  $x=$_SESSION['des'][0]-=1;
+  $d=$_SESSION['des'][1];
     if($x==$d){
       return $d;
     }elseif($x>$d){
@@ -38,10 +48,10 @@ function right(){
 }
 echo right();
 echo right();
-echo right();
-echo right();
-//echo right();
-//echo right();
+echo left();
+//echo left();
+// echo right();
+// echo right();
 // function up(){
 //   $y+1;
 // }
