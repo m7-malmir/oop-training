ca418fb8fce24a6ff26abf0897e726a612b6cb6e
Mohamed Malmir <98361393+m7-malmir@users.noreply.github.com>
2023-11-11 12:48:07 +0330

    Update home.php
    simple compact php

--- home.php
+++ home.php
@@ -1,3 +1,41 @@
 <?php 
 
 
+
+trait DroneTrait{
+    private $first;
+    private $des;
+    public function __construct($first,$des){
+        $this->first=$first;
+        $this->des=$des;
+    } 
+
+    public function getFirstPoint{
+	  $stri = (string)$this->first;
+    $x = $stri[0];
+    $y = $stri[1];
+    $stri=[$x,$y];
+    $_SESSION['fip']=$stri;
+    return $_SESSION['fip'];
+    }
+
+    public function getDesPoint{
+    $strz = (string)$this->des;
+    $j = $strz[0];
+    $k = $strz[1];
+    $strz=[$j,$k];
+    $_SESSION['sep']=$strz;
+    return $_SESSION['sep'];
+    }
+  public funcrion getTodoMove{
+  
+
+  }
+}
+
+class Drone{
+    use DroneTrait;
+}
+
+
+?>
