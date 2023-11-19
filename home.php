fb2a7efd8a0544ce670d2837b059d1df831caa40
Mohamed Malmir <98361393+m7-malmir@users.noreply.github.com>
2023-11-19 16:01:52 +0330

    Update home.php

--- home.php
+++ home.php
@@ -5,21 +5,15 @@ class Car {
     public $des;
     public $fip;
     public $sep;
-    public function __construct($first,$des,$fuel){
-        $this->first=$first;
-        $this->des=$des;
-        //$_SESSION['fuel']=$fuel;
-              if(!isset($_SESSION['fip'])){ 
-                   $this->fip = $_SESSION['fip'];
-                   session_start();
-               }      
+    public function __construct($first){
+        $this->first=$first;      
     }
     public function right(){ 
          $stri =(string)$this->first; 
          $x = $stri[0];
          $y = $stri[1];
          $stri=[$x,$y];
-         $this->fip=$stri;
+         $_SESSION['fip']=$stri;
          $_SESSION['fip'][0]+=1;
          return $_SESSION['fip'];
       }
@@ -28,6 +22,15 @@ $newcar=new Car(23,44,14);
 //print_r($_SESSION['fip']);
 print_r($newcar->right());
 print_r($newcar->right());
-//print_r($newcar->right());
+print_r($newcar->right());
 //print_r($_SESSION);
 ?>
+
+
+
+
+
+
+
+
+
