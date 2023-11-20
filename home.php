d30c687b0e9e34e06e8523061847ea70646f8409
Mohammad Malmir <m.malmir666@gmail.com>
2023-11-20 22:18:53 +0330

    problem with session is solved

--- home.php
+++ home.php
@@ -1,28 +1,32 @@
 <?php 
 //session_destroy();
+session_start();
 class Car {
     public $first;
-    public $des;
-    public $fip;
-    public $sep;
     public function __construct($first){
         $this->first=$first;      
+          $stri =(string)$this->first; 
+          $x = $stri[0];
+          $y = $stri[1];
+          $stri=[$x,$y];
+          $_SESSION['fip']=$stri;
+        
     }
     public function right(){ 
-         $stri =(string)$this->first; 
-         $x = $stri[0];
-         $y = $stri[1];
-         $stri=[$x,$y];
-         $_SESSION['fip']=$stri;
+        //  $stri =(string)$this->first; 
+        //  $x = $stri[0];
+        //  $y = $stri[1];
+        //  $stri=[$x,$y];
+        //  $_SESSION['fip']=$stri;
          $_SESSION['fip'][0]+=1;
-         return $_SESSION['fip'];
+          return  $_SESSION['fip'];
       }
 }
-$newcar=new Car(23,44,14);
+$newcar=new Car(23);
 //print_r($_SESSION['fip']);
 print_r($newcar->right());
-print_r($newcar->right());
-print_r($newcar->right());
+ print_r($newcar->right());
+ print_r($newcar->right());
 //print_r($_SESSION);
 ?>
 
