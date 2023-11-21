9051f3fe9c82822faeaac9e7ba82f4e2ac16e20c
Mohamed Malmir <98361393+m7-malmir@users.noreply.github.com>
2023-11-21 21:29:25 +0330

    Update home.php

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //session_destroy();
 session_start();
 class Car {
@@ -10,7 +10,21 @@ class Car {
           $y = $stri[1];
           $stri=[$x,$y];
           $_SESSION['fip']=$stri;
-        
+$stri = (string)$xy;
+    $x = $stri[0];
+    $y = $stri[1];
+    $stri=[$x,$y];
+    $_SESSION['fip']=$stri;
+    $strz = (string)$z;
+    $j = $strz[0];
+    $k = $strz[1];
+    $strz=[$j,$k];
+    $_SESSION['sep']=$strz;
+    $a= $j-$x;
+    $b= $k-$y;
+    $des=[$a,$b];
+    $_SESSION['des']=$des;
+
     }
     public function right(){ 
         //  $stri =(string)$this->first; 
@@ -28,13 +42,3 @@ print_r($newcar->right());
  print_r($newcar->right());
  print_r($newcar->right());
 //print_r($_SESSION);
-?>
-
-
-
-
-
-
-
-
-
