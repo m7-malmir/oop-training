73fa4d062a3099d3131c9e902e08ca21045a8f70
Mohamed Malmir <98361393+m7-malmir@users.noreply.github.com>
2023-11-22 18:50:05 +0330

    Update home.php

--- home.php
+++ home.php
@@ -1,21 +1,20 @@
-<?php
+<?php 
 //session_destroy();
 session_start();
 class Car {
-    public $first;
-    public function __construct($first){
-        $this->first=$first;      
+public $first;
+public $des;
+public $fuel;
+public function __construct($first,$des,$fuel){
+        $this->first=$first;   
+        $this->des=$des;
+        $this->fuel=$fuel;
           $stri =(string)$this->first; 
           $x = $stri[0];
           $y = $stri[1];
           $stri=[$x,$y];
           $_SESSION['fip']=$stri;
-$stri = (string)$xy;
-    $x = $stri[0];
-    $y = $stri[1];
-    $stri=[$x,$y];
-    $_SESSION['fip']=$stri;
-    $strz = (string)$z;
+        $strz = (string)$z;
     $j = $strz[0];
     $k = $strz[1];
     $strz=[$j,$k];
@@ -24,21 +23,35 @@ $stri = (string)$xy;
     $b= $k-$y;
     $des=[$a,$b];
     $_SESSION['des']=$des;
-
     }
     public function right(){ 
-        //  $stri =(string)$this->first; 
-        //  $x = $stri[0];
-        //  $y = $stri[1];
-        //  $stri=[$x,$y];
-        //  $_SESSION['fip']=$stri;
          $_SESSION['fip'][0]+=1;
           return  $_SESSION['fip'];
       }
+public function up(){ 
+         $_SESSION['fip'][1]+=1;
+          return  $_SESSION['fip'];
+      }
 }
-$newcar=new Car(23);
-//print_r($_SESSION['fip']);
+$newcar=new Car(23,56,88);
+print_r($_SESSION['fip']);
 print_r($newcar->right());
- print_r($newcar->right());
- print_r($newcar->right());
+ print_r($newcar->up());
+ //print_r($newcar->right());
 //print_r($_SESSION);
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
