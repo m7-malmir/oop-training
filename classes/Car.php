07239d91ad4befee37a702bebb84f01be0f2a4d6
Mohammad Malmir <m.malmir666@gmail.com>
2023-12-18 01:53:12 +0330

    home update

--- classes/Car.php
+++ classes/Car.php
@@ -9,12 +9,6 @@ abstract class Car{
         $this->fuel=$f;
     }
      
-function benz($a,$b,$fuel){
-  global $flag_benz;
-  $flag_benz=1;
-  start($a,$b);
-  $_SESSION['fuel']=$fuel;
-}
 
 
 }
--- home.php
+++ home.php
@@ -2,31 +2,51 @@
 //session_destroy();
 session_start();
 class Car {
-    public $first;
-    public function __construct($first){
+    public $first;  public $des;
+    public $fuel;
+    public function __construct($first,$des,$fuel){
         $this->first=$first;      
           $stri =(string)$this->first; 
           $x = $stri[0];
           $y = $stri[1];
           $stri=[$x,$y];
           $_SESSION['fip']=$stri;
-        
+
+          $this->des=$des;      
+          $strz =(string)$this->des; 
+          $x = $strz[0];
+          $y = $strz[1];
+          $strz=[$x,$y];
+          $_SESSION['des']=$strz;
+
+          $this->fuel=$fuel;
+          $_SESSION['fuel']=$fuel;
     }
     public function right(){ 
-        //  $stri =(string)$this->first; 
-        //  $x = $stri[0];
-        //  $y = $stri[1];
-        //  $stri=[$x,$y];
-        //  $_SESSION['fip']=$stri;
-         $_SESSION['fip'][0]+=1;
+        $_SESSION['fuel']=$_SESSION['fuel']-3.8;
+         $x=$_SESSION['fip'][0]+=1;
+         $d=$_SESSION['des'][0];
+           if($x==$d){
+             echo 'u arrived';
+           }elseif($x>$d){
+              echo 'u passed destination turn back';
+           }else{
+              echo 'u are in right way';
+           }
+          return  $_SESSION['fip'];
+      }
+      public function up(){ 
+         $_SESSION['fip'][1]+=1;
           return  $_SESSION['fip'];
       }
 }
-$newcar=new Car(23);
-//print_r($_SESSION['fip']);
+$newcar=new Car(23,55,10);
+print_r($_SESSION['fip']);
 print_r($newcar->right());
- print_r($newcar->right());
- print_r($newcar->right());
+//print_r($newcar->right());
+//  print_r($newcar->right());
+//  print_r($newcar->up());
+//  print_r($newcar->up());
 //print_r($_SESSION);
 ?>
 
