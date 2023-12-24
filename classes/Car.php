b4f19b2192c50fd87947e9405602124d6fa91935
Mohammad Malmir <m.malmir666@gmail.com>
2023-12-24 23:22:34 +0330

    edit file for coorection trait

--- classes/Car.php
+++ classes/Car.php
@@ -21,45 +21,7 @@ class Car {
           $_SESSION['des']=$strz;
           $_SESSION['fuel']=$fuel;
     }
-    public function right(){ 
-        $_SESSION['fuel']=$_SESSION['fuel']-$this->fuel;
-        if( $_SESSION['fuel'] >= 0){
-         $x=$_SESSION['fip'][0]+=1;
-         $d=$_SESSION['des'][0];
-         $y=$_SESSION['fip'][1];
-         $b=$_SESSION['des'][1];
-           if($x==$d && $y==$b){
-             echo 'u arrived';
-           }elseif($x>$d){
-              echo 'u passed destination turn back';
-           }else{
-              echo 'u are in right way';
-           }
-        }else{
-         echo 'your fuel is finish';
-        }
-        
-          return  $_SESSION['fip'];
-    }
-    public function up(){ 
-        $_SESSION['fuel']=$_SESSION['fuel']-$this->fuel;
-        if( $_SESSION['fuel'] >= 0){
-         $y=$_SESSION['fip'][0];
-         $b=$_SESSION['des'][0];
-        $x=$_SESSION['fip'][1]+=1;
-        $d=$_SESSION['des'][1];
-        if($x==$d && $y==$b){
-            echo 'u arrived';
-          }elseif($x>$d){
-             echo 'u passed destination turn back';
-          }else{
-             echo 'u are in right way';
-          }
-         }else{
-            echo 'your fuel is finish';
-           }
-          return  $_SESSION['fip'];
-    }
+    use carTrait;
 }
 
 ?>
--- classes/Ferrari.php
+++ classes/Ferrari.php
@@ -1,6 +1,6 @@
 <?php
-
-class Ferrari{
+include 'Car.php';
+class Ferrari extends Car{
 
     use carTrait;
     public $fuel=5;
